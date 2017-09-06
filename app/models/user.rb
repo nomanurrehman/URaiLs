c33bbb19ea0846f16ed0c29c514b8df5b191d0ce
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-09-06 19:58:19 +0500

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable
+
+  # validations
+  validates_presence_of :first_name, :last_name, :date_of_birth
+  validates_length_of :first_name, maximum: 50
+  validates_length_of :last_name, maximum: 50
+  
+  # associations
+  has_many :ownerships
+  has_many :urls, through: :ownerships
+end
