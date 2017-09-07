b72e93fc239c3516abfbb226a50abacbaab787d1
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-09-07 12:54:01 +0500

    Add application scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,55 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require turbolinks
+
+// bootstrap
+//= require adminbsb-materialdesign/plugins/bootstrap/js/bootstrap.min.js
+
+// slim scroll
+//= require adminbsb-materialdesign/plugins/jquery-slimscroll/jquery.slimscroll.min.js
+
+// node waves
+//= require adminbsb-materialdesign/plugins/node-waves/waves.min.js
+
+// count to
+//= require adminbsb-materialdesign/plugins/jquery-countto/jquery.countTo.js
+
+// raphael
+//= require adminbsb-materialdesign/plugins/raphael/raphael.min.js
+
+// morrisjs
+//= require adminbsb-materialdesign/plugins/morrisjs/morris.min.js
+
+// chartjs
+//= require adminbsb-materialdesign/plugins/chartjs/Chart.bundle.min.js
+
+// flot chart
+//= require adminbsb-materialdesign/plugins/flot-charts/jquery.flot.js
+//= require adminbsb-materialdesign/plugins/flot-charts/jquery.flot.resize.js
+//= require adminbsb-materialdesign/plugins/flot-charts/jquery.flot.pie.js
+//= require adminbsb-materialdesign/plugins/flot-charts/jquery.flot.categories.js
+//= require adminbsb-materialdesign/plugins/flot-charts/jquery.flot.time.js
+
+// sparkline chart
+//= require adminbsb-materialdesign/plugins/jquery-sparkline/jquery.sparkline.js
+
+// moment js
+//= require adminbsb-materialdesign/plugins/momentjs/moment.js
+
+// material datepicker
+//= require adminbsb-materialdesign/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js
+
+// admin js
+//= require adminbsb-materialdesign/js/admin.js
+
 //= require_tree .
+
+// initialize material date picker
+// set date format / don't show time
+$('.datepicker').bootstrapMaterialDatePicker({
+  format: 'YYYY-MM-DD',
+  time: false
+});
