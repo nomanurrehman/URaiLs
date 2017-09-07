b853912695822a86368d10090440e9ade5ce3ae8
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-09-07 21:08:24 +0500

    Update url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -8,4 +8,16 @@ class Url < ApplicationRecord
   # should be a valid/reachable url
   validates_presence_of :path
   validates_with UrlValidator
+
+  # generates short code for url
+  def generate_code
+    # calculation for code length
+    # (required_length * 3) / 4
+    length = (8 * 3) / 4
+    # keep generating code
+    # if it exists in database already
+    begin
+      self.code = SecureRandom.urlsafe_base64(length)
+    end while Url.exists?(code: self.code)
+  end
 end
